06cc79fb516d4338b134f832ebfa23d15146e956
Chaitanya-Mistry <ambicanagar17@gmail.com>
2021-08-08 10:23:24 +0530

    Voice Feature Added
    Whenever user try to delete individual list or All list items he/she will be alerted using voice feature

--- logic.js
+++ logic.js
@@ -1,7 +1,19 @@
+function say(m) {
+    let msg = new SpeechSynthesisUtterance();
+    msg.voice = window.speechSynthesis.getVoices()[4];
+    msg.voiceURI = "native";
+    msg.volume = 1;
+    msg.rate = 1;
+    msg.pitch = 1.1;
+    msg.text = m;
+    msg.lang = 'en-US';
+    speechSynthesis.speak(msg);
+  }
+
 list = document.getElementsByClassName('individual_list')[0];
-input = document.getElementById('list')
-add = document.getElementById('add')
-pendingContainer = document.getElementsByClassName('pending')[0]
+input = document.getElementById('list');
+add = document.getElementById('add');
+pendingContainer = document.getElementsByClassName('pending')[0];
 
 // List Container
 listContainer = document.getElementsByClassName('listContainer')[0];
@@ -10,7 +22,7 @@ let lcsArray;
 let AllLists;
 
 showNotes();
-
+let checks = document.getElementsByClassName('checks');
 add.addEventListener('click', () => {
 
     if (localStorage.getItem('todo') == null) {
@@ -24,8 +36,8 @@ add.addEventListener('click', () => {
         listContainer.html = `<h3>Please Enter Something...</h3>`;
     }
     else {
-        lcsArray.push(input.value)
-        input.value = ""
+        lcsArray.push(input.value);
+        input.value = "";
 
         // Convert a JavaScript object into a string with JSON.stringify().When sending data to a web server, the data has to be a string..
         localStorage.setItem('todo', JSON.stringify(lcsArray))
@@ -46,13 +58,12 @@ function showNotes() {
         lcsArray = JSON.parse(localStorage.getItem('todo'));
 
         for (let i = 0; i < lcsArray.length; i++) {
-            html += `<p class="individual_list" onmouseover="showDelete(${i})">${lcsArray[i]}<span class="material-icons delete_icon" onclick="deleteIT(${i})">delete</span></p>`;
+            html += `<p class="individual_list" onmouseover="showDelete(${i})">${lcsArray[i]}<span class="material-icons delete_icon" onclick="deleteIT(${i})" contenteditable="false">delete</span></p>`;
         }
         listContainer.innerHTML = html;
 
         if (lcsArray.length == 1) {
-            pendingContainer.innerHTML = `<h3 id="pendingTasks">You have only <span style="color:red">${lcsArray.length}</span> pending task !</h3>
-            <button id="clearAll" onclick="clearALL()">Clear All</button>`;
+            pendingContainer.innerHTML = `<h3 id="pendingTasks">You have only <span style="color:red">${lcsArray.length}</span> pending task !</h3>`;
             hideDustbin();
         }
         else {
@@ -64,7 +75,9 @@ function showNotes() {
 }
 // Delete individual list Function
 function deleteIT(no) {
-    confirmation = confirm("It will we deleted permenantly ! , Do you want to contiue ? ")
+    say("Do you reallyyy want to delete it ?..");
+    confirmation = confirm("It will we deleted permenantly ! , Do you want to contiue ? ");
+
     if (confirmation) {
         lcsArray = JSON.parse(localStorage.getItem('todo'));
         lcsArray.splice(no, 1);
@@ -81,7 +94,9 @@ function deleteIT(no) {
 }
 // Delete all items in list
 function clearALL() {
+    say("warning , It will delete all the list items...");
     confirmation = confirm("All items of the list will be deleted...");
+
     if (confirmation) {
         // To remove the specific item from the localStorage
         localStorage.removeItem('todo');
